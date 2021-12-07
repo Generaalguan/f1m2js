892862ed700fbf7ea7ee8be005307247fc79788c
Generaalguan <33372@ma-web.nl>
2021-12-07 11:30:25 +0100

    first commit

---
+++ web/les2-boodschappen/script.js
@@ -0,0 +1,12 @@
+const groentevak = document.getElementById ("groente");
+const fruitvak = document.getElementById ("fruit");
+
+function zetinfruitla(fruit){
+    console.log("zetinfruitla");
+
+}
+
+function zetingroentela(){
+    console.log("zetingroentela");
+    
+}
